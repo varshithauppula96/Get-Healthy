2f6fb8b47c3bfdfb01b3c4f671208da13b25badd
Varshitha Uppula <varshithauppula@gmail.com>
2021-04-17 00:14:37 -0400

    login userpage

--- src/App.js
+++ src/App.js
@@ -15,6 +15,29 @@ import UserStories from "./components/home-anonymous/user-stories";
 import Trainer from "./components/trainer/trainer-home";
 import { Provider } from "react-redux";
 import store from "./store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import PrivateRoute from "./private-route/privateRoute";
+import Dashboard from "./components/dashboard/dashboard";
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+    // Set auth token header auth
+    const token = localStorage.jwtToken;
+    setAuthToken(token);
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(token);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+    const currentTime = Date.now() / 1000; // to get in milliseconds
+    if (decoded.exp < currentTime) {
+        // Logout user
+        store.dispatch(logoutUser());
+        // Redirect to login
+        window.location.href = "./login";
+    }
+}
 function App() {
     return (
         <Provider store={store}>
@@ -34,6 +57,10 @@ function App() {
                 <Route path="/details/:uri" exact={true}>
                     <DetailsScreen/>
                 </Route>
+                {/* eslint-disable-next-line react/jsx-no-undef */}
+
+                    <PrivateRoute exact path="/dashboard" component={Dashboard} />
+
                 <Route path="/trainer" exact={true}  component={Trainer}/>
             </div>
 
