fa23ac0680083ff983b7600169a5ca1be51304b6
Srihitha <36701923+srihitha97@users.noreply.github.com>
2021-04-26 19:29:37 -0400

    Update profile.js

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -3,7 +3,10 @@ import {Link} from 'react-router-dom';
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {logoutUser} from "../../actions/authActions";
+import Home_user_services from "../../services/home_user_services";
+
 class Profile extends Component {
+
     constructor() {
         super();
         this.state = {
@@ -11,28 +14,55 @@ class Profile extends Component {
         }
         this.state = {
             userId:"",
-            username:"",
+            trainerId:"",
+            name:"",
             userType:"",
             email:"",
-            redirect:false
+            gender:"",
+            dateOfBirth:"",
+            weightInKgs:"",
+            heightInCms:"",
+            about:"",
+            trainer:{}
         }
     }
+
     componentDidMount(){
+
         this.setState({
             userId: this.props.auth.user._id,
-            username: this.props.auth.name,
-            userType: this.props.auth.userType,
-            email:this.props.auth.email
+            trainerId: this.props.auth.user.trainerId,
+            name: this.props.auth.user.name,
+            userType: this.props.auth.user.userType,
+            email:this.props.auth.user.email,
+            gender:this.props.auth.user.gender,
+            dateOfBirth:this.props.auth.user.dateOfBirth,
+            weightInKgs:this.props.auth.user.weightInKgs,
+            heightInCms:this.props.auth.user.heightInCms,
+            about:this.props.auth.user.about,
         })
+        Home_user_services.findUserById(this.props.auth.user.trainerId)
+            .then(t=>this.setState({trainer:t}))
+
     }
 
-    handleInputChange = event => {
-        const value = event.target.value;
-        const name = event.target.name;
+    updateDetails = () => {
+        this.setState({editing:false})
+        console.log()
+    }
+
+    handleInputChange = (value) => {
+
         this.setState({
-            [name]: value
+            name: value,
+            gender: value,
+            weightInKgs: value,
+            heightInCms: value,
+            about: value
         });
     };
+
+
     // handleUpdate = () => {
     //     var loginUser = {
     //         username: this.state.username.trim(),
@@ -49,81 +79,149 @@ class Profile extends Component {
     //             }))}
     // };
 
+
     render () {
-        const { user } = this.props.auth;
-        console.log("User===" + this.user);
+
+        const user  = this.props.auth;
+
         const { classes } = this.props;
 
+        const userUpdated = {
+            name: this.state.name,
+            gender: this.state.gender,
+            weightInKgs: this.state.weightInKgs,
+            heightInCms: this.state.heightInCms,
+            about:this.state.about,
+            email:this.state.email,
+            trainerId: this.state.trainerId,
+            dateOfBirth: this.state.dateOfBirth,
+            userType: this.state.userType
+        }
+
+        const updateUsers =() => {
+            this.setState({editing:false})
+            console.log(userUpdated)
+            //updateUser.updateDetails()
+        }
         return(
+
             <div className='container-fluid'>
                 <h1>Profile</h1>
                 {
-                    user===undefined &&
-                <>
-                    {
-                        alert("Not logged In")
-                    }
-                    {/*<div className='alert alert-warning'>*/}
-                    {/*    Not logged in*/}
-                    {/*</div>*/}
-                    {/*<Link className='btn btn-outline-primary' to='/login'>Go to login page</Link>*/}
-                </>
+                    user.isAuthenticated==false&&
+                    <div>
+                        <div className='alert alert-warning'>
+                            Not logged in
+                        </div>
+                        <Link className='btn btn-outline-primary' to='/login'>Go to login page</Link>
+                    </div>
                 }
                 {
-                    user && !this.state.editing &&
+                    user.isAuthenticated==true && !this.state.editing &&
                     <div>
                         <h3>User Information</h3>
                         <button className='btn btn-outline-primary float-right' onClick={() => this.setState({editing:true})}>Edit profile</button>
                         <ul className='list-group mt-5'>
-                            <li className='list-group-item'>Display Name: {user.name}</li>
-                            <li className='list-group-item'>Username: {user.email}</li>
-                            <li className='list-group-item'>Profile ID: {user._id}</li>
-                            <li className='list-group-item'>Role: {user.userType}</li>
+                            <li className='list-group-item'>Display Name : {user.user.name}</li>
+                            <li className='list-group-item'>Username : {user.user.email}</li>
+                            <li className='list-group-item'>Gender : {user.user.gender}</li>
+                            <li className='list-group-item'>DoB : {user.user.dateOfBirth}</li>
+                            <li className='list-group-item'>Weight(Kgs) : {user.user.weightInKgs}</li>
+                            <li className='list-group-item'>Height(Cms) : {user.user.heightInCms}</li>
+                            <li className='list-group-item'>Bio : {user.user.about}</li>
+                            <li className='list-group-item'>
+                                Your Trainer : &nbsp;
+                                <Link to={`/profile/${this.props.auth.user.trainerId}`}>
+                                    {this.state.trainer["name"]}
+                                </Link>
+                            </li>
                         </ul>
                     </div>
                 }
                 {
-                    user && this.state.editing &&
+                    user.isAuthenticated==true && this.state.editing &&
                     <div>
                         <ul className='list-group'>
                             <li className='list-group-item'>Display Name:
                                 <input className='form-control'
-                                       value={this.state.name}
-                                       onChange={this.handleInputChange}
+                                       defaultValue={this.state.name}
+                                       onChange={
+                                           (e)=>{
+                                               this.setState({name:e.target.value})
+                                           }
+                                       }
                                 />
                             </li>
-                            <li className='list-group-item'>Username: {user.email} (cannot change)</li>
-                            <li className='list-group-item'>New password:
-                                <input className='form-control'
-                                       type='password'
-                                       value={this.state.password}
-                                       onChange={this.handleInputChange}
-                                />
-                            </li>
-                            <li className='list-group-item'>Profile ID: {user._id} (cannot change)</li>
-                            <li className='list-group-item'>Role:
-                                <select className='form-control' onChange={this.handleInputChange} value={this.state.userType}>
-                                    <option value='user'>User</option>
-                                    <option value='superuser'>Trainer</option>
+                            <li className='list-group-item'>Email: {user.user.email}</li>
+                            <li className='list-group-item'>Role: {user.user.userType}</li>
+                            <li className='list-group-item'>DoB: {user.user.dateOfBirth}</li>
+
+                            <li className='list-group-item'>Gender:
+                                <select className='form-control'
+                                        onChange={
+                                            (e)=>{
+                                                this.setState({gender:e.target.value})
+                                            }
+                                        }
+                                        value={user.user.gender}>
+                                    <option value='user'>Male</option>
+                                    <option value='superuser'>Female</option>
                                 </select>
                             </li>
+                            <li className='list-group-item'>Weight(Kgs):
+                                <input type="number"
+                                       className='form-control'
+                                       defaultValue={this.state.weightInKgs}
+                                       onChange={
+                                           (e)=>{
+                                               this.setState({weightInKgs:e.target.value})
+                                           }
+                                       }
+                                />
+                            </li>
+                            <li className='list-group-item'>Height(Cms):
+                                <input type="number"
+                                       className='form-control'
+                                       defaultValue={this.state.heightInCms}
+                                       onChange={
+                                           (e)=>{
+                                               this.setState({heightInCms:e.target.value})
+                                           }
+                                       }
+                                />
+                            </li>
+                            <li className='list-group-item'>Bio:
+                                <input
+                                    type="text"
+                                    className='form-control'
+                                    defaultValue={this.state.about}
+                                    onChange={
+                                        (e)=>{
+                                            this.setState({about:e.target.value})
+                                        }
+                                    }
+                                />
+                            </li>
                         </ul>
+                        <button
+                            onClick={updateUsers}
+                            className="btn btn-primary btn-block">Update</button>
                     </div>
-                }
-                {
-                    JSON.stringify(user)
                 }
             </div>
         )
     }
 }
+
 Profile.propTypes = {
     logoutUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired
 };
+
 const mapStateToProps = state => ({
     auth: state.auth
 });
+
 export default connect(
     mapStateToProps,
     { logoutUser }
