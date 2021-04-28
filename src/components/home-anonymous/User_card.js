0c011d5886a59c4494a3c070a66a88745552308f
Varshitha Uppula <varshithauppula@gmail.com>
2021-04-28 12:54:19 -0400

    removed unwanted code

--- src/components/home-anonymous/User_card.js
+++ src/components/home-anonymous/User_card.js
@@ -3,38 +3,15 @@ import {Link} from "react-router-dom";
 const UserCard = (
     {
         user,
-        lastModified="1/1/2021",
-        owner="who knows?",
-        deleteUser,
-        updateUser
     }) => {
-    const [editing, setEditing] = useState(false)
-    const [name, setName] = useState(user.name)
-    const saveUser = () => {
-        setEditing(false)
-        const newUser = {
-            ...user,
-            name: name
-        }
-        updateUser(newUser)
-    }
+
     return (
         <div className="col-xl-4 col-lg-3 col-md-4 col-sm-6 col-xs-1">
             <div className="card" >
                 {<img className="card-img-top" src="https://picsum.photos/300/200" />}
                 <div className="card-body" >
                     <h5 className="card-title">
-                        {!editing &&
-                        // <a href={`/profile/${user._id}`}>
                         <h3>{user.name}</h3>
-                        // </a>
-                        }
-                        {editing &&
-                        <input
-                            className="form-control "
-                            onChange={(e) => setName(e.target.value)}
-                            value={name}/>
-                        }
                     </h5>
                     <p className="card-text">{user.about}</p>
                    <p><b>Weight:</b> {user.weightInKgs}kg</p>
